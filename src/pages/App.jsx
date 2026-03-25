5c5a35518808eed955e3d58ee9a54b3f840aefa0
SooulDay04 <rore040120@itsuruapan.edu.mx>
2026-03-25 15:59:59 -0600

    Se agrego un componente para que cuando se cambie la pagina aparezca hsta arriba al inicio

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeController from "./home/HomeController";
 import ShopController from "./shop/ShopController";
+import ScrollToTop from "../components/ScrollTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Routes>
         <Route path="/" element={<HomeController />} />
         <Route path="/tienda" element={<ShopController />} />
